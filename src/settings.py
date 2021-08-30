50e3539575e77491b7c396b6cfc7a5a2d37de813
Anton Bahayeuski <antnbog@gmail.com>
2021-08-30 19:52:05 +0400

    jwt one minute

--- src/settings.py
+++ src/settings.py
@@ -146,7 +146,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 CORS_ALLOWED_ORIGINS = [
