54b2ee7618bec749eb9cfb8d878314be0aec3d24
Anton Bahayeuski <antnbog@gmail.com>
2021-09-15 18:21:32 +0400

    Fix element plus

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,7 +5,7 @@
 <style lang="scss">
 @import '~@fortawesome/fontawesome-free/css/all.min.css';
 @import "~bootstrap-icons/font/bootstrap-icons.css";
-@import "~element-plus/lib/theme-chalk/index.css";
+@import "~element-plus/theme-chalk/index.css";
 @import '~flatpickr/dist/flatpickr.css';
 @import "~sweetalert2/dist/sweetalert2.css";
 
