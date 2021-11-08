3a915e78e490c286904913ca54df274c7edf4065
abogaevski <antnbog@gmail.com>
2021-11-08 22:26:40 +0400

    Small mod in Project Model

--- projects/models.py
+++ projects/models.py
@@ -6,20 +6,18 @@ from core.constants import *
 class Project(models.Model):
 
     title = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=9, decimal_places=2)
     description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
     time_range = models.PositiveIntegerField()
-
     is_active = models.BooleanField(default=True)
+    color = models.CharField(max_length=255, choices=COLORS, default=PRIMARY)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='projects')
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    color = models.CharField(max_length=255, choices=COLORS, default=PRIMARY)
-
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='projects')
 
     class Meta:
         verbose_name = 'Project'
         verbose_name_plural = 'Projects'
         db_table = 'projects'
+        ordering = ['-pk']
