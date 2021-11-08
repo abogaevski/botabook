898d4fbf5324ff1100e0e1ef81d76a4e4290b16b
abogaevski <antnbog@gmail.com>
2021-11-08 14:11:14 +0400

    Project auto create

--- accounts/signals.py
+++ accounts/signals.py
@@ -3,7 +3,9 @@ from django.dispatch import receiver
 from django.template.defaultfilters import slugify
 from rest_framework.authtoken.models import Token
 
+from projects.models import Project
 from .models import User, Profile
+from core.constants import PRIMARY
 
 
 @receiver(post_save, sender=User, dispatch_uid='save_new_user_profile')
@@ -23,3 +25,17 @@ def save_profile(sender, instance, created, **kwargs):
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def create_initial_project(sender, instance=None, created=False, **kwargs):
+    if created:
+        Project.objects.create(
+            title='Бесплатная встреча на пол часа',
+            description='Эта встреча предназначена для предварительной связи со мной.',
+            price=0,
+            time_range=30,
+            is_active=True,
+            color=PRIMARY,
+            user=instance
+        )
